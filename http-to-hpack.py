40e987ab542ed9ca3659ff1a1f5051f2aa0f39f8
Brendan Long <b.long@cablelabs.com>
2015-05-29 12:10:20 -0600

    Support reading multiple header files for more real-world comparisons.

--- http-to-hpack.py
+++ http-to-hpack.py
@@ -12,15 +12,10 @@ def shuffle_string(string):
     return "".join(l)
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("http_header_files", nargs="+")
-    args = parser.parse_args()
-
+def read_headers(filename):
     size = 0
     headers = []
-
-    with open(args.http_header_file) as f:
+    with open(filename) as f:
         for line in f:
             size += len(line) + 1
             if not line:
@@ -29,35 +24,24 @@ if __name__ == "__main__":
                 method, path, _ = line.split(" ")
                 headers.append((":method", method))
                 headers.append((":path", path))
-                first = False
             else:
                 headers.append([v.strip() for v in line.split(":", maxsplit=1)])
+    return size, headers
 
-    print("Header size for HTTP/1.1: %s bytes" % size)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("http_header_files", nargs="+")
+    args = parser.parse_args()
 
     encoder = hpack.Encoder()
-    encoded_headers = [encoder.add((key.encode("UTF-8"), value.encode("UTF-8")))
-                       for key, value in headers]
-    print("Header size for HTTP/2 on first request: %s bytes" % sum(map(len, encoded_headers)))
 
-    encoded_headers = [encoder.add((key.encode("UTF-8"), value.encode("UTF-8")))
-                       for key, value in headers]
-    print("Header size for HTTP/2 on subsequent requests with same path: %s bytes" % sum(map(len, encoded_headers)))
+    for i, filename in enumerate(args.http_header_files, start=1):
+        http1_size, headers = read_headers(filename)
 
-    # change id in query string
-    o = urlparse(path)
-    query = parse_qs(o.query)
-    if "id" in query:
-        query["id"] = shuffle_string(query["id"])
-        query = {key: value[0] for key, value in query.items()}
-        headers[1] = ":path", urlunparse((o.scheme, o.netloc, o.path, o.params, urlencode(query), o.fragment))
+        print("Header size for header %s in HTTP/1.1: %s bytes" % (i, http1_size))
+
         encoded_headers = [encoder.add((key.encode("UTF-8"), value.encode("UTF-8")))
                            for key, value in headers]
-        print("Header size for HTTP/2 on subsequent requests with similar path: %s bytes" % sum(map(len, encoded_headers)))
-
-    # completely change path
-    headers[1] = ":path", shuffle_string(path)
-    encoded_headers = [encoder.add((key.encode("UTF-8"), value.encode("UTF-8")))
-                       for key, value in headers]
-    print("Header size for HTTP/2 on subsequent requests with completely different path: %s bytes" % \
-        sum(map(len, encoded_headers)))
+        print("Header size for header %s in HTTP/2: %s bytes" % (i, sum(map(len, encoded_headers))))
+        print()
