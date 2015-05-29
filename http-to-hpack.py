4bf7c2cfc32ecade9391c4fbfb6a290520d2aa36
Brendan Long <b.long@cablelabs.com>
2015-05-29 17:02:17 -0600

    Remove header sorting since it makes no difference in HPACK.

--- http-to-hpack.py
+++ http-to-hpack.py
@@ -35,7 +35,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     encoder = hpack.Encoder()
-    sorted_encoder = hpack.Encoder()
 
     for i, filename in enumerate(args.http_header_files, start=1):
         print(filename)
@@ -46,16 +45,4 @@ if __name__ == "__main__":
         encoded_headers = [encoder.add((key.encode("UTF-8"), value.encode("UTF-8")))
                            for key, value in headers]
         print("Header size for headers %s in HTTP/2: %s bytes" % (i, sum(map(len, encoded_headers))))
-
-        # Sort query string to make it easier to compress repeatedly
-        path = headers[1][1]
-        parts = urlparse(path)
-        query = parse_qs(parts.query)
-        sorted_query = [(key, value[0]) for key, value in sorted(query.items())]
-        sorted_query.sort(key=lambda x: x[0] == "id")
-        headers[1] = ":path", urlunparse(("", "", parts.path, "", urlencode(sorted_query), ""))
-
-        encoded_headers = [sorted_encoder.add((key.encode("UTF-8"), value.encode("UTF-8")))
-                           for key, value in headers]
-        print("Header size for headers (using sorted query string) %s in HTTP/2: %s bytes" % (i, sum(map(len, encoded_headers))))
         print()
